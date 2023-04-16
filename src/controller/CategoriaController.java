51337424f6fb314c207e0f612e514c00529d9b18
RafaelM <rafael.mattos09@gmail.com>
2023-04-16 18:15:34 -0300

    controller_view

--- src/controller/CategoriaController.java
+++ src/controller/CategoriaController.java
@@ -1,16 +1,21 @@
 package controller;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
+import dao.CategoriaDAO;
+import factory.ConnectionFactory;
 import model.Categoria;
 
 public class CategoriaController {
+    private CategoriaDAO categoriaDAO;
 
+    public CategoriaController() {
+        Connection connection = new ConnectionFactory().recuperarConexao();
+        this.categoriaDAO = new CategoriaDAO(connection);
+    }
     public List<Categoria> listar() {
-        List<Categoria> categorias =
-                new ArrayList<Categoria>();
-        categorias.add(new Categoria(1, "Categoria de teste"));
-        return categorias;
+        return this.categoriaDAO.listar();
     }
 }
